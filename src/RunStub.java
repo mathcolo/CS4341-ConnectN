9732bf53c12e30f29b9bede525231bb210336583
Preston Mueller <skierjunk@gmail.com>
2016-01-17 22:46:46 -0500

    Update project xmls and some other java stuff

--- src/RunStub.java
+++ src/RunStub.java
@@ -10,14 +10,15 @@ public class RunStub {
 
     public static void main(String[] args) throws Exception {
 
-        String command_to_execute_player1="java TheresaPrestonPlayer";
-        String command_to_execute_player2="java -jar testPlayer.jar";
+        String command_to_execute_player1="java -jar testPlayer.jar";
+        String command_to_execute_player2="java TheresaPrestonPlayer";
         int board_height=7;
         int board_width=7;
         int N=4;
         int announceNameTimeLimit =5;
         int moveTimeLimit=2;
         Referee ref=new Referee( command_to_execute_player1, command_to_execute_player2, board_height, board_width,  N,  announceNameTimeLimit,  moveTimeLimit);
+
 
         ref.start();
     }
--- src/TheresaPrestonPlayer.java
+++ src/TheresaPrestonPlayer.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 /**
  * Created by prestonmueller on 1/17/16.
  */
 public class TheresaPrestonPlayer {
 
     public static void main(String[] args ){
-        System.out.println("Hello world!");
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
