7a04aa9577817f4257b9b16efe6464871276447b
Preston Mueller <skierjunk@gmail.com>
2016-01-19 20:20:37 -0500

    May have implemented Minimax (w/out AB prune) correctly, but hard say ;)

--- src/MinimaxBoard.java
+++ src/MinimaxBoard.java
@@ -14,35 +14,68 @@ public class MinimaxBoard {
 		this.parentMove = parentMove;
 	}
 	
-	public int generateChildren(int depth) {
+	public int minimax(int depth, int player) {
 		
 		if(board == null)
 			System.err.println("Cannot generate children, board was null!");
 		
-		if(depth == 0)
-			return 0;
+		if(depth == 0) {
+			//Insert real heuristic evaluation function here
+			return 6;
+		}
 		
-		int totalSub = 0;
-		
-		//Iterate through each column of the board
-		for(int i = 0; i < board.getWidth(); i++) {
-			if(board.canDropADiscFromTop(i, parentMove.player)) {
-				//Move can be done to this column
+		//Max
+		if(player == 1) {
+			
+			int maximumValue = Integer.MIN_VALUE;
+			
+			//Iterate through each column of the board
+			for(int i = 0; i < board.getWidth(); i++) {
+				if(board.canDropADiscFromTop(i, parentMove.player)) {
+					//Move can be done to this column
 
-				Move move = new Move(i, Move.oppositePlayer(parentMove.player), Move.Type.DROP);
-				MinimaxBoard newBoard = new MinimaxBoard(new Board(this.board), move);
-				newBoard.board.dropADiscFromTop(i, move.player);
-				this.children.add(newBoard);
-				totalSub++;
-				
-				totalSub += newBoard.generateChildren(depth-1);
-				
+					Move move = new Move(i, player, Move.Type.DROP);
+					MinimaxBoard newBoard = new MinimaxBoard(new Board(this.board), move);
+					newBoard.board.dropADiscFromTop(i, move.player);
+					this.children.add(newBoard);
+
+					int value = newBoard.minimax(depth-1, Move.oppositePlayer(player));
+					if(value > maximumValue)
+						maximumValue = value;
+				}
 				
 			}
 			
+			return maximumValue;
+			
 		}
-		
-		return totalSub;
+		//Min
+		else if(player == 2) {
+			
+			int minimumValue = Integer.MAX_VALUE;
+			
+			//Iterate through each column of the board
+			for(int i = 0; i < board.getWidth(); i++) {
+				if(board.canDropADiscFromTop(i, parentMove.player)) {
+					//Move can be done to this column
+
+					Move move = new Move(i, player, Move.Type.DROP);
+					MinimaxBoard newBoard = new MinimaxBoard(new Board(this.board), move);
+					newBoard.board.dropADiscFromTop(i, move.player);
+					this.children.add(newBoard);
+
+					int value = newBoard.minimax(depth-1, Move.oppositePlayer(player));
+					if(value < minimumValue)
+						minimumValue = value;
+				}
+				
+			}
+			
+			return minimumValue;
+			
+		}
+
+		return -1;
 	}
 	
 	
@@ -51,10 +84,10 @@ public class MinimaxBoard {
 		Board test = new Board(7, 7, 4);
 		test.dropADiscFromTop(2, 1);
 		
-		
-		MinimaxBoard wrapper = new MinimaxBoard(test, new Move(2,1,Move.Type.DROP));
-		wrapper.generateChildren(5);
-		wrapper.children.get(2).board.printBoard();
+		//1 = max
+		MinimaxBoard wrapper = new MinimaxBoard(test, new Move(2,2,Move.Type.DROP));
+		int value = wrapper.minimax(5, 2);
+		System.out.println("Minimax value: " + value);
 		
 		
 	}
